e11ba2bba1d8c0fe49293d4abab0c885bbc5add7
Andy Saml <lander@i.ua>
2019-06-03 20:50:54 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(factor(5))
+	fmt.Println(factorTail(5, 1))
+	fmt.Println(factorTailCover(5))
+}
---
+++ recursions.go
@@ -0,0 +1,28 @@
+package main
+
+func factor(x int) int {
+	if x == 1 {
+		return x
+	}
+	return x * factor(x-1)
+}
+
+func factorTail(x, acc int) int {
+	acc *= x
+	if x == 1 {
+		return acc
+	}
+	return factorTail(x-1, acc)
+
+}
+
+func factorTailCover(n int) int {
+	factorTail := func(x, acc int) int {
+		acc *= x
+		if x == 1 {
+			return acc
+		}
+		return factorTail(x-1, acc)
+	}
+	return factorTail(n, 1)
+}
