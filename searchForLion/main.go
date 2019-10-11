747c530bef3ca07e667dce6397bc80b8792a82e2
Andrii Samoilenko <Andrii.Samoilenko@genesys.com>
2019-10-11 15:57:53 +0300

    2

--- searchForLion/main.go
+++ searchForLion/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
 )
 
@@ -14,15 +15,21 @@ func init() {
 var letters = []rune("abcdefghijklmnopqrstuvwxyz")
 var bigStr string
 
-go func randSeq(n int) string {
+func randSeq(n int) string {
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(b)
-}()
+}
 
 func main() {
-	str := randSeq(10e2)
-	fmt.Println(str)
+	str := randSeq(10e4)
+	// fmt.Println(str)
+
+	if strings.Contains(str, "loin") {
+		fmt.Println("got lion!")
+	} else {
+		fmt.Println("no lion in str...")
+	}
 }
