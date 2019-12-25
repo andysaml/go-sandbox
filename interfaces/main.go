ab155d28906bb86d688ae454c48f210e7684c96d
Andy Saml <lander@i.ua>
2019-12-25 19:55:56 +0200

    interfaces

--- interfaces/main.go
+++ interfaces/main.go
@@ -12,17 +12,17 @@ type Sound interface {
 	say()
 }
 
-func (cat Cat) say() {
+func (cat *Cat) say() {
 	fmt.Println("meoy!")
 }
 
-func (dog Dog) say() {
+func (dog *Dog) say() {
 	fmt.Println("bark!")
 }
 
 func main() {
-	a := Cat{}
-	b := Dog{}
+	a := &Cat{}
+	b := &Dog{}
 	arr := []Sound{a, b}
 
 	for _, item := range arr {
